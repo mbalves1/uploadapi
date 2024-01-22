a85b4d29ee4bfd80ed3459e8f052843ee08a7004
Murilo <mbalves1@outlook.com>
2024-01-22 17:49:14 -0300

    [feat]: adicionando verificacao para data e adicionando ao banco

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -4,8 +4,6 @@ import * as XLSX from 'xlsx';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FileEntity } from './entities/file.entity';
 import { Repository } from 'typeorm';
-import { format } from 'date-fns';
-import * as moment from 'moment';
 
 @Injectable()
 export class FilesService {
@@ -23,28 +21,32 @@ export class FilesService {
         quantidadeCobrancas: file['quantidade cobranças'] as number,
         cobradaACadaXDias: file['cobrada a cada X dias'] as number,
         dataInicio:
-          typeof file['data início'] === 'string'
+          typeof file['data início'] !== 'string' ||
+          isNaN(Date.parse(file['data início']))
             ? new Date(Date.parse(file['data início']))
             : file['data início'],
         status: file['status'] as string,
         dataStatus:
-          typeof file['data status'] === 'string'
+          typeof file['data status'] !== 'string' ||
+          isNaN(Date.parse(file['data status']))
             ? new Date(Date.parse(file['data status']))
             : file['data status'],
         dataCancelamento:
-          file['data cancelamento'] === undefined
+          file['data cancelamento'] === undefined ||
+          isNaN(Date.parse(file['data cancelamento']))
             ? null
             : new Date(Date.parse(file['data cancelamento'])),
         valor: file['valor'].toString(),
         proximoCiclo:
-          typeof file['data início'] === undefined
+          file['próximo ciclo'] === undefined ||
+          isNaN(Date.parse(file['próximo ciclo']))
             ? null
             : new Date(Date.parse(file['próximo ciclo'])),
 
         idAssinante: file['ID assinante'] as string,
       };
 
-      // this.fileRepository.save(obj);
+      this.fileRepository.save(obj);
 
       return obj;
     });
